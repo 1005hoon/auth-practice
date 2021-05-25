ef0b71d0eb9751c72d209332965f44b13b26ba48
Hoon <shoh@help-me.kr>
2021-05-25 10:24:03 +0900

    add basic router

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const http = require("http");
 const morgan = require("morgan");
+const router = require("./router");
 
 const app = express();
 
 app.use(express.json());
 app.use(morgan("dev"));
+
+router(app);
 
 const server = http.createServer(app);
 
